48b034a998e410e1921fb14c662063325c8ecb85
fahlanlogic <alanfahdi@gmail.com>
2023-10-29 06:47:04 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const server = express();
+mongoose
+	.connect(process.env.MONGO)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
 server.listen(3000, () => {
-  console.log(`Server is running on port 3000!!`)
-})
+	console.log(`Server is running on port 3000`);
+});
