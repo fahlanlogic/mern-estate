9995750c356cbf04f5db7cb4523bdde6d311e19a
fahlanlogic <alanfahdi@gmail.com>
2023-10-30 20:38:05 +0700

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom"
 export default function Header() {
   return (
     <header>
-      <div className="flex justify-between items-center p-3 bg-slate-200">
+      <div className="flex justify-between items-center p-3 bg-slate-white shadow-lg">
         <h1>
           <Link to="/">
           <span className='text-black font-black'>Fahlan</span>
           <span className='text-slate-600 font-bold'>Estate</span>
           </Link>
         </h1>
-        <form className="flex items-center p-2 bg-slate-300 rounded-lg">
+        <form className="flex items-center p-2 border rounded-lg">
           <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-36 sm:w-64"/>
           <FaSearch />
         </form>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,20 @@
 // import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="max-w-xl mx-auto p-3">
+      <h1 className="text-3xl font-bold text-center p-3 mb-6">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input type="text" id="username" placeholder="Username" className="border p-2 rounded-xl" />
+        <input type="email" id="email" placeholder="Email" className="border p-2 rounded-xl" />
+        <input type="password" id="password" placeholder="Password" className="border p-2 rounded-xl" />
+        <button className="bg-black text-white shadow-lg rounded-xl p-2 font-semibold hover:bg-white hover:text-black duration-300 disabled:bg-slate-600">Sign Up</button>
+      </form>
+      <div className='flex gap-2 justify-center mt-6'>
+        <p>Already have an account?</p>
+        <Link to={"/sign-in"}><span className='text-blue-600'>Sign In</span></Link>
+      </div>
+    </div>
   )
 }
