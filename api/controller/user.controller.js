09e9890375abeb45fd8b1dc71931cb626725adcd
fahlanlogic <alanfahdi@gmail.com>
2023-12-08 22:14:03 +0700

    create get user listings api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 // membuat variable untuk mengembalikan respon
 export const test = (req, res) => {
@@ -59,5 +60,20 @@ export const deleteUser = async (req, res, next) => {
 		res.status(200).json("User has been deleted!");
 	} catch (error) {
 		next(error.message);
+	}
+};
+
+export const getUserListings = async (req, res, next) => {
+	if (req.user.id === req.params.id) {
+		try {
+			const listings = await Listing.find({
+				userRef: req.params.id,
+			});
+			res.status(200).json(listings);
+		} catch (error) {
+			next(error);
+		}
+	} else {
+		next(errorHandler(401, "You can only delete your own account!"));
 	}
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,20 @@
 import express from "express";
-import { deleteUser, test, updateUser } from "../controller/user.controller.js";
+import {
+	deleteUser,
+	getUserListings,
+	test,
+	updateUser,
+} from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 // buat router dari express
-const user = express.Router();
+const router = express.Router();
 
 // memanggil dari route path /test yang isinya dari user.controller.js (best practicenya controller jangan langsung digabung dengan route)
-user.get("/test", test);
-user.post("/update/:id", verifyToken, updateUser);
-user.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 // export dan gunakan di index.js agar bisa digunakan
-export default user;
+export default router;
