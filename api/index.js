ee55067708f6a487eaeaf1d4caac631c0c2dcedb
fahlanlogic <alanfahdi@gmail.com>
2024-06-12 20:19:47 +0700

    add build and test route

--- api/index.js
+++ api/index.js
@@ -11,26 +11,32 @@ dotenv.config();
 
 // inisialisasi express dengan variable
 const app = express();
+const router = express.Router();
 
 app.use(express.json());
 app.use(cookieParser());
 
 // menghubungkan server dengan database mongoDB menggunakan perintah connect dan lakukan percobaan dengan then/catch
 mongoose
-	.connect(process.env.MONGO)
-	.then(() => {
-		console.log("Connected to MongoDB");
-	})
-	.catch(err => {
-		console.log(err);
-	});
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // inisialisasi port untuk server
 app.listen(3000, () => {
-	console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port 3000`);
 });
 
 // inisialisasi route untuk back end api/user
+router.get("/", (req, res) => {
+  res.json({
+    message: "API is working",
+  });
+});
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
