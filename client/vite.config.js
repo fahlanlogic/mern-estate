8e92c02eea1bb143161b2b2bd364520deb438fcf
fahlanlogic <alanfahdi@gmail.com>
2024-06-12 21:16:29 +0700

    config vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:3000",
-				secure: false,
-			},
-		},
-	},
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
 
-	plugins: [react()],
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
