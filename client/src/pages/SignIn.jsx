daa3c8beaafe91c7030ada316207b1d44ee9fc89
fahlan logic <alanfahdi@gmail.com>
2024-06-12 16:59:51 +0700

    edit font size

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -20,79 +20,74 @@ export default function SignIn() {
     })
   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    dispatch(signInStart()) // disable button
+  const handleSubmit = async e => {
+    e.preventDefault();
+    dispatch(signInStart()); // disable button
     // lakukan try catch untuk antisipasi kemungkinan error
     try {
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
-      })
+        body: JSON.stringify(formData),
+      });
       const data = await res.json();
-      console.log(data)
+      console.log(data);
       if (data.succes === false) {
-        dispatch(signInFailure(data.message))
+        dispatch(signInFailure(data.message));
         alert(data.message);
         return;
       }
-      dispatch(signInSucces(data))
+      dispatch(signInSucces(data));
       navigate("/");
     } catch (error) {
-      dispatch(signInFailure())
+      dispatch(signInFailure());
     }
-  }
-  console.log(formData);
+  };
 
   return (
-		<main>
-			<div className="hero min-h-screen px-4 md:w-5/6 max-w-2xl mx-auto flex items-center">
-				<div className="w-full">
-					<h1 className="text-3xl font-bold mb-2 text-slate-700 md:text-4xl">
-						Selamat datang di Airbnb
-					</h1>
-					<h1 className="text-md mb-12 text-slate-700">
-						Mendaftar atau masuk?
-					</h1>
-					<form
-						className="flex flex-col gap-4"
-						onSubmit={handleSubmit}>
-						<input
-							type="email"
-							id="email"
-							placeholder="Email"
-							className="border px-3 py-2 rounded-full"
-							onChange={handleChange}
-						/>
-						<input
-							type="password"
-							id="password"
-							placeholder="Password"
-							className="border p-2 mb-5 px-3 py-2 rounded-full"
-							onChange={handleChange}
-						/>
-						<button
-							disabled={loading}
-							className="bg-gradient-to-b from-pink-500 to-pink-600 text-white shadow-lg rounded-full p-2 font-semibold hover:opacity-90 duration-300 disabled:bg-pink-300">
-							{loading ? "Loading..." : "Sign In"}
-						</button>
-						<OAuth />
-					</form>
-					<div className="flex gap-2 justify-center mt-6">
-						<p className="text-slate-700">
-							Dont have an account?
-						</p>
-						<Link to={"/sign-up"}>
-							<span className="text-blue-600">
-								Sign Up
-							</span>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</main>
+    <main>
+      <div className="hero min-h-screen px-4 md:w-5/6 max-w-2xl mx-auto flex items-center">
+        <div className="w-full">
+          <h1 className="text-3xl font-bold mb-2 text-slate-800 md:mb-4 md:text-6xl">
+            Selamat datang di <span className="text-pink-600">Airbnb</span>
+          </h1>
+          <h1 className="text-md mb-12 text-slate-700 md:text-xl">
+            Mendaftar atau masuk?
+          </h1>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit}>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="border px-3 py-2 rounded-full"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="border p-2 mb-5 px-3 py-2 rounded-full"
+              onChange={handleChange}
+            />
+            <button
+              disabled={loading}
+              className="bg-gradient-to-b from-pink-500 to-pink-600 text-white shadow-lg rounded-full p-2 font-semibold hover:opacity-90 duration-300 disabled:bg-pink-300">
+              {loading ? "Loading..." : "Sign In"}
+            </button>
+            <OAuth />
+          </form>
+          <div className="flex gap-2 justify-center mt-6">
+            <p className="text-slate-700">Dont have an account?</p>
+            <Link to={"/sign-up"}>
+              <span className="text-blue-600">Sign Up</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -48,58 +48,54 @@ export default function SignUp() {
   console.log(formData);
 
   return (
-		<main>
-			<div className="hero min-h-screen px-4 md:w-5/6 max-w-2xl mx-auto flex items-center">
-				<div className="w-full">
-					<h1 className="text-3xl font-bold mb-2 text-slate-700 md:text-4xl">
-						Selamat datang di Airbnb
-					</h1>
-					<h1 className="text-md mb-12 text-slate-700">
-						Mendaftar atau masuk?
-					</h1>
-					<form
-						className="flex flex-col gap-4"
-						onSubmit={handleSubmit}>
-						<input
-							type="text"
-							id="username"
-							placeholder="Username"
-							className="border px-3 py-2 rounded-full"
-							onChange={handleChange}
-						/>
-						<input
-							type="email"
-							id="email"
-							placeholder="Email"
-							className="border px-3 py-2 rounded-full"
-							onChange={handleChange}
-						/>
-						<input
-							type="password"
-							id="password"
-							placeholder="Password"
-							className="border p-2 mb-5 px-3 py-2 rounded-full"
-							onChange={handleChange}
-						/>
-						<button
-							disabled={loading}
-							className="bg-gradient-to-b from-pink-500 to-pink-600 text-white shadow-lg rounded-full p-2 font-semibold hover:opacity-90 duration-300 disabled:bg-pink-300">
-							{loading ? "Loading..." : "Sign Up"}
-						</button>
-						<OAuth />
-					</form>
-					<div className="flex gap-2 justify-center mt-6">
-						<p className="text-slate-700">
-							Already have an account?
-						</p>
-						<Link to={"/sign-in"}>
-							<span className="text-blue-600">
-								Sign In
-							</span>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</main>
+    <main>
+      <div className="hero min-h-screen px-4 md:w-5/6 max-w-2xl mx-auto flex items-center">
+        <div className="w-full">
+          <h1 className="text-sm font-bold mb-2 text-black md:text-4xl">
+            Selamat datang di Airbnbbbb
+          </h1>
+          <h1 className="text-md mb-12 text-slate-700">
+            Mendaftar atau masuk?
+          </h1>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id="username"
+              placeholder="Username"
+              className="border px-3 py-2 rounded-full"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="border px-3 py-2 rounded-full"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="border p-2 mb-5 px-3 py-2 rounded-full"
+              onChange={handleChange}
+            />
+            <button
+              disabled={loading}
+              className="bg-gradient-to-b from-pink-500 to-pink-600 text-white shadow-lg rounded-full p-2 font-semibold hover:opacity-90 duration-300 disabled:bg-pink-300">
+              {loading ? "Loading..." : "Sign Up"}
+            </button>
+            <OAuth />
+          </form>
+          <div className="flex gap-2 justify-center mt-6">
+            <p className="text-slate-700">Already have an account?</p>
+            <Link to={"/sign-in"}>
+              <span className="text-blue-600">Sign In</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
