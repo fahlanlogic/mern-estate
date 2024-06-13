cf9ade4eff92a7d4efd66fa3c80281565f712e05
fahlan logic <alanfahdi@gmail.com>
2024-06-13 13:26:54 +0700

    update config vercel and vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
         target: "https://mern-estate-silk.vercel.app",
         // secure: false,
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ""),
+        rewrite: path => path.replace(/^\/api/, "/api"),
       },
     },
-  },
-
-  plugins: [react()],
-  build: {
-    outDir: "dist",
   },
 });
