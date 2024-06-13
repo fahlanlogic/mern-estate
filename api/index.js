b903c3d2312df6a1d181988e00221ef0bae51ad5
fahlan logic <alanfahdi@gmail.com>
2024-06-13 15:46:21 +0700

    fix route api in client

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+// import cors from "cors";
 
 // inisialisasi dari package dotenv agar bisa dipakai
 dotenv.config();
@@ -16,7 +16,7 @@ const router = express.Router();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 
 // menghubungkan server dengan database mongoDB menggunakan perintah connect dan lakukan percobaan dengan then/catch
 mongoose
